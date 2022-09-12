86523d717c1b21050278bec2f3e5bd124a73f563
Yoga Worksheet <yoga.worksheet@gmail.com>
2022-09-12 11:36:04 +0700

    install dotenv

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize("express-api", "yoga", "mysql2022", {
-    host: 'localhost',
-    dialect: 'mysql'
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB
 });
 
 module.exports = sequelize;
