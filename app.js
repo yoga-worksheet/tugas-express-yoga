7f76b37a26c7ff63ab4ebeaaf134ca6c0ebb203d
Yoga Worksheet <yoga.worksheet@gmail.com>
2022-09-23 14:41:17 +0700

    enabling v2 (sequelize)

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ app.use("/public", express.static(path.join(__dirname, "uploads")));
 // Routes Module
 const index = require("./Routes/index");
 // const productRouter = require("./app/product/routes");
-// const productRouterV2 = require("./app/product-v2/routes");
+const productRouterV2 = require("./app/product-v2/routes");
 const productRouterV3 = require("./app/product-v3/routes");
 const productRouterV4 = require("./app/product-v4/routes");
 
 // Routes Use
 app.use(index);
 // app.use("/api/v1", productRouter);
-// app.use("/api/v2", productRouterV2);
+app.use("/api/v2", productRouterV2);
 app.use("/api/v3", productRouterV3);
 app.use("/api/v4", productRouterV4);
 app.use((req, res) => {
